0dc1bc8bac1725b046877ded6820f9bd4f758516
Alexander <alexandergaruba96@gmail.com>
2019-06-15 22:34:00 +0100

    Landing Page Update

---
+++ landing/footer.php
@@ -0,0 +1,35 @@
+<button id="myBtn" class="btn waves-effect waves-light" type="button" style="display: inline-block;">
+  <i class="icon ion-ios-arrow-up text-white"></i>
+</button>
+<footer class="footer-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <img src="/img/logo.png" alt="Lucid" class="img-fluid pb-3" />
+      </div>
+      <div class="col-lg-12 text-center">
+        <p>&copy; 2019 Lucid. All rights reserved.</p>
+      </div>
+      <div class="col-lg-12 text-center">
+        <ul class="footer-links">
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Support</a></li>
+          <li><a href="/terms-and-conditions.php">Terms and Condition</a></li>
+          <li><a href="/privacy-policy.php">Privacy policy</a></li>
+          <li><a href="/get-started.php">Get Started</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="/js/script.js"></script>
+</body>
+
+</html>
---
+++ landing/get-started.php
@@ -0,0 +1,46 @@
+<?php include 'header.php' ?>
+
+<section class="page-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="pb-5">Getting Started</h1>
+            </div>
+            <div class="col-md-12 pb-3">
+                <h2>Installation</h2>
+                <ul>
+                    <li class="pb-2">Download lucid by clicking <a href="https://download.getlucid.app/lucid.zip"
+                            target="_blank">here</a></li>
+                    <li class="pb-2">Login to your domain CPanel and upload the zipped lucid file.</li>
+                    <li class="pb-2">Unzip the file to the /www directory or your root directory.</li>
+                    <li class="pb-2">Visit your domain on your browser and install lucid by using your gmail or facebook
+                        account, or entering your details in the form.</li>
+                    <li class="pb-2">Click install and you are set</li>
+                </ul>
+            </div>
+            <span class="w-100 border border-dark"></span>
+            <div class="col-md-12 pt-5">
+                <h2>FAQs</h2>
+                <ul>
+                    <li class="pb-2">
+                        <em><b>Q: What is the difference "microblog" and "blog" mode?</b></em><br>
+                        A: Microblog mode is typically for small content. It doesn't have a title or the What You See Is
+                        What You Get (WYSIWYG) Editor. The blog is for larger content, features a title field and the
+                        WYSIWYG Editor.
+                    </li>
+                    <li class="pb-2">
+                        <em><b>Q: How do I switch or toggle microblog/blog modes?</b></em><br>
+                        A: The default mode is microblog. Blog is automatically toggled when the input field exceeds 100
+                        characters. There is no other way to switch modes.
+                    </li>
+                    <li class="pb-2">
+                        <em><b>Q: How can a guest visit or access my page?</b></em><br>
+                        A: After installing Lucid on your custom domain, simply share your domain URL with others.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php' ?>
