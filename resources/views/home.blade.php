d9631c3b8094e37571abc69bc76ab48dfee3e25e
JeffOgah <jeff.ogah@gmail.com>
2019-07-08 22:46:00 +0100

    timeline update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,117 +1,133 @@
 @extends('layouts.lucid')
 @section('sidebar')
-    @parent
+@parent
 
 @endsection
 @section('content')
-			<!-- Beginning of Post Content -->
-				<div class="post-content">
-					<div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
-						<img src="{{ asset('img/post-1.jpg') }}" class="img-fluid post-img"
-							alt="What I think of Donald Glover’s New Video" />
-					</div>
-					<div class="post-content-body">
-						<p class="post-date">3rd April, 2019</p>
-						<h3 class="post-title">
-							What I think of Donald Glover’s New Video
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet, consectetur....
-						</p>
-					</div>
-				</div>
+<!-- Editor -->
+<p>Write a Post</p>
+<form class="mb-3">
+  <div class="form-row mb-3">
+    <div class="col-7">
+      <input type="text" class="form-control" placeholder="Title">
+    </div>
+    <div class="col-4 offset-md-1">
+      <input type="file" class="custom-file-input" id="customFile">
+      <label class="custom-file-label" for="customFile">Choose file</label>
+    </div>
+  </div>
+  <div class="form-group">
+    <textarea type="text" class="form-control" placeholder="Tell your story"></textarea>
+  </div>
+  <div class="text-right">
+  <button type="submit" class="btn bg-secondary text-white">Submit</button>
+  </div>
+</form>
+<!-- End Editor -->
+<!-- Beginning of Post Content -->
+<div class="post-content">
+  <div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
+    <img src="{{ asset('img/post-1.jpg') }}" class="img-fluid post-img" alt="What I think of Donald Glover’s New Video" />
+  </div>
+  <div class="post-content-body">
+    <p class="post-date">3rd April, 2019</p>
+    <h3 class="post-title">
+      What I think of Donald Glover’s New Video
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet, consectetur....
+    </p>
+  </div>
+</div>
 
-				<div class="post-content">
-					<div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
-						<img src="{{ asset('img/post-2.jpg') }}" class="img-fluid post-img"
-							alt="Looking For Where To Spend Christmas in the comform of your home" />
-					</div>
-					<div class="post-content-body">
-						<p class="post-date">2nd April, 2019</p>
-						<h3 class="post-title">
-							Looking For Where To Spend Christmas in the comform of your home
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet....
-						</p>
-					</div>
-				</div>
+<div class="post-content">
+  <div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
+    <img src="{{ asset('img/post-2.jpg') }}" class="img-fluid post-img" alt="Looking For Where To Spend Christmas in the comform of your home" />
+  </div>
+  <div class="post-content-body">
+    <p class="post-date">2nd April, 2019</p>
+    <h3 class="post-title">
+      Looking For Where To Spend Christmas in the comform of your home
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet....
+    </p>
+  </div>
+</div>
 
-				<div class="post-content">
-					<div class="post-content-body">
-						<p class="post-date">1st April, 2019</p>
-						<h3 class="post-title">
-							Looking For Where To Spend Christmas in the comform of your home
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet....
-						</p>
-					</div>
-				</div>
+<div class="post-content">
+  <div class="post-content-body">
+    <p class="post-date">1st April, 2019</p>
+    <h3 class="post-title">
+      Looking For Where To Spend Christmas in the comform of your home
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet....
+    </p>
+  </div>
+</div>
 
-				<div class="post-content">
-					<div class="post-content-body">
-						<p class="post-date">1st April, 2019</p>
-						<h3 class="post-title">
-							Looking For Where To Spend Christmas in the comform of your home
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet....
-						</p>
-					</div>
-				</div>
+<div class="post-content">
+  <div class="post-content-body">
+    <p class="post-date">1st April, 2019</p>
+    <h3 class="post-title">
+      Looking For Where To Spend Christmas in the comform of your home
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet....
+    </p>
+  </div>
+</div>
 
-				<div class="post-content">
-					<div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
-						<img src="{{ asset('img/post-1.jpg') }}" class="img-fluid post-img"
-							alt="What I think of Donald Glover’s New Video" />
-					</div>
-					<div class="post-content-body">
-						<p class="post-date">3rd April, 2019</p>
-						<h3 class="post-title">
-							What I think of Donald Glover’s New Video
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet, consectetur....
-						</p>
-					</div>
-				</div>
+<div class="post-content">
+  <div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
+    <img src="{{ asset('img/post-1.jpg') }}" class="img-fluid post-img" alt="What I think of Donald Glover’s New Video" />
+  </div>
+  <div class="post-content-body">
+    <p class="post-date">3rd April, 2019</p>
+    <h3 class="post-title">
+      What I think of Donald Glover’s New Video
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet, consectetur....
+    </p>
+  </div>
+</div>
 
-				<div class="post-content">
-					<div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
-						<img src="{{ asset('img/post-2.jpg') }}" class="img-fluid post-img"
-							alt="Looking For Where To Spend Christmas in the comform of your home" />
-					</div>
-					<div class="post-content-body">
-						<p class="post-date">2nd April, 2019</p>
-						<h3 class="post-title">
-							Looking For Where To Spend Christmas in the comform of your home
-						</h3>
-						<p class="post-body">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
-							dolor sit amet....
-						</p>
-					</div>
-				</div>
+<div class="post-content">
+  <div class="post-image d-none d-lg-flex d-xl-flex d-md-flex">
+    <img src="{{ asset('img/post-2.jpg') }}" class="img-fluid post-img" alt="Looking For Where To Spend Christmas in the comform of your home" />
+  </div>
+  <div class="post-content-body">
+    <p class="post-date">2nd April, 2019</p>
+    <h3 class="post-title">
+      Looking For Where To Spend Christmas in the comform of your home
+    </h3>
+    <p class="post-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore , Lorem ipsum
+      dolor sit amet....
+    </p>
+  </div>
+</div>
 
-				<div class="text-center">
-					<button class="btn btn-primary pagination">
-						Previous articles <i class="pl-2 icon ion-ios-arrow-forward"></i>
-					</button>
-			</div>
+<div class="text-center">
+  <button class="btn btn-primary pagination">
+    Previous articles <i class="pl-2 icon ion-ios-arrow-forward"></i>
+  </button>
+</div>
 
-			<!-- End of Post Content -->
+<!-- End of Post Content -->
 <!-- @foreach ($posts as $feeds)
 @if ($feeds['img'] ="")
 <div class="post-content">
