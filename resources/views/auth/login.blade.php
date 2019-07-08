ab12c86904a4f4c611757fab5bfac0dd7825ba54
JeffOgah <jeff.ogah@gmail.com>
2019-07-08 22:46:55 +0100

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
                 <div class="form-group">
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Your email address">
                 </div>
-                <button type="submit" class="btn bg-secondary text-white w-100">Continue with Email</button>
+                <button type="submit" class="btn bg-alt text-white w-100">Continue with Email</button>
             </form>
             <div class="text-right mt-2">
                 <a href="" class="text-left text-secondary">Forgot Password?</a>
