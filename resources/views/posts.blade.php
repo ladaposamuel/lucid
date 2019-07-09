9584b388068b9e0465fea8a3fec4aa7bfcc82fb5
JeffOgah <jeff.ogah@gmail.com>
2019-07-09 02:05:27 +0100

    posts update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,40 +5,58 @@
 @endsection
 @section('content')
 
- <!-- Begin content -->
+<!-- Begin content -->
 <div class="post-content border-top border-info pt-3 mt-3">
-<img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
+  <img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
   <div class="post-content-body ml-3">
-      <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
+    <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
       <p class="">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
       </p>
-      <span>
-      <i class="icon ion-md-chatboxes p-1"> 3</i>
-</span>
-    </div>
+      <div>
+        <p>
+          <i class="icon ion-md-happy p-1"> 3</i>
+          <i class="icon ion-md-sad p-1"> 3</i>
+          <i class="icon ion-md-chatboxes p-1"> 3</i>
+        </p>
+      </div>
+  </div>
 </div>
 
 <div class="post-content border-top border-info pt-3 mt-3">
-<img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
+  <img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
   <div class="post-content-body ml-3">
-      <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
+    <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
       <p class="">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
       </p>
       <img src="{{ asset('img/my-post.png') }}" alt="" class="">
-    </div>
+      <div class="mt-3">
+        <p>
+          <i class="icon ion-md-happy p-1"> 3</i>
+          <i class="icon ion-md-sad p-1"> 3</i>
+          <i class="icon ion-md-chatboxes p-1"> 3</i>
+        </p>
+      </div>
+  </div>
 </div>
 
 <div class="post-content border-top border-info pt-3 mt-3">
-<img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
+  <img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
   <div class="post-content-body ml-3">
-      <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
+    <h6 class="font-weight-bold">{{Auth::user()->name}} <small class="text-muted">- March 28, 2019 </small></h5>
       <p class="">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
       </p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/VSbUJt7dhNU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
+      <div class="mt-3">
+        <p>
+          <i class="icon ion-md-happy p-1"> 3</i>
+          <i class="icon ion-md-sad p-1"> 3</i>
+          <i class="icon ion-md-chatboxes p-1"> 3</i>
+        </p>
+      </div>
+  </div>
 </div>
 <!-- End content -->
 @endsection
