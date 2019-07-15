4ec6f4c87fb3b10d3e8d0719b8a6f4f5c5b121f0
chibuikeIg <paulchibiukeigweze@gmail.com>
2019-07-15 09:13:23 -0700

    fixed bug on dropdown not working and set auth for post page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -208,6 +208,7 @@
   }
 </style>
 <!-- The editor code goes here -->
+@auth
 <div id="form-container">
     <form method="POST" class="timeline-editor" id="editor-form" autocomplete="OFF">
       <div class=" row pt-3 pb-3">
@@ -291,6 +292,8 @@
     </form>
   </div>
 <!-- Beginning of Post Content -->
+@endauth
+
 @forelse($posts as $post)
 
 
@@ -352,10 +355,7 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
   integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
   <script src="https://cdn.quilljs.com/1.3.4/quill.js"></script>
   <!-- Convert to markdown script -->
 <script src="https://unpkg.com/turndown/dist/turndown.js"></script>
