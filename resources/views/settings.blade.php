2fde9e7fd9a97aefd7e6a1dbbf3fb73fc23eee17
JeffOgah <jeff.ogah@gmail.com>
2019-07-16 11:34:19 +0100

    setting tab navigation fixed

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -3,85 +3,45 @@
 @parent
 @endsection
 @section('content')
- <!-- beginning of settings page -->
-  <div class="settings">
-    <!-- tab navigation here -->
-    <h4><strong>Settings</strong></h4>
-    <ul class="nav nav-tabs navbar-light" id="settings-tabs" role="tablist">
-      <li class="nav-item">
-        <a
-          href="#profile"
-          class="nav-link tab-link active"
-          id="profile-tab"
-          data-toggle="tab"
-          role="tab"
-          aria-controls="profile"
-          aria-selected="true"
-        >
-          <h6>Profile</h6>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a
-          href="#account"
-          class="nav-link tab-link"
-          id="account-tab"
-          data-toggle="tab"
-          role="tab"
-          aria-controls="account"
-          aria-selected="false"
-        >
-          <h6>Account</h6>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a
-          href="#links"
-          class="nav-link tab-link"
-          id="links-tab"
-          data-toggle="tab"
-          role="tab"
-          aria-controls="links"
-          aria-selected="false"
-        >
-          <h6>Links</h6>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a
-          href="#security"
-          class="nav-link tab-link"
-          id="security-tab"
-          data-toggle="tab"
-          role="tab"
-          aria-controls="links"
-          aria-selected="false"
-        >
-          <h6>Security</h6>
-        </a>
-      </li>
-    </ul>
-    <!-- tab content here -->
-    <div class="tab-content" id="settings-tabs-content">
-      <!-- profile settings tab -->
-      <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-        <form action="" class="mt-5">
-          <div class="row">
-            <div class="form-group col-sm-12 col-md-6">
-              <label for="name"><strong>Your Name</strong></label>
-              <input class="form-control" type="text" name="name" id="name" value="" placeholder="Jon Champion" />
-            </div>
-            <div class="form-group col-sm-12 col-md-6">
-              <label for="email"><strong>Your Email Address</strong></label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                class="form-control"
-                value=""
-                placeholder="example@gmail.com"
-              />
-            </div>
+<!-- beginning of settings page -->
+<div class="settings">
+  <!-- tab navigation here -->
+  <h4><strong>Settings</strong></h4>
+  <ul class="nav nav-tabs navbar-light" id="settings-tabs" role="tablist">
+    <li class="nav-item">
+      <a href="#profile" class="nav-link tab-link active" id="profile-tab" data-toggle="tab" role="tab" aria-controls="profile" aria-selected="true">
+        <h6>Profile</h6>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#account" class="nav-link tab-link" id="account-tab" data-toggle="tab" role="tab" aria-controls="account" aria-selected="false">
+        <h6>Account</h6>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#links" class="nav-link tab-link" id="links-tab" data-toggle="tab" role="tab" aria-controls="links" aria-selected="false">
+        <h6>Links</h6>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#security" class="nav-link tab-link" id="security-tab" data-toggle="tab" role="tab" aria-controls="links" aria-selected="false">
+        <h6>Security</h6>
+      </a>
+    </li>
+  </ul>
+  <!-- tab content here -->
+  <div class="tab-content" id="settings-tabs-content">
+    <!-- profile settings tab -->
+    <div class="tab-pane fade in show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+      <form action="" class="mt-5">
+        <div class="row">
+          <div class="form-group col-sm-12 col-md-6">
+            <label for="name"><strong>Your Name</strong></label>
+            <input class="form-control" type="text" name="name" id="name" value="" placeholder="Jon Champion" />
+          </div>
+          <div class="form-group col-sm-12 col-md-6">
+            <label for="email"><strong>Your Email Address</strong></label>
+            <input type="email" name="email" id="email" class="form-control" value="" placeholder="example@gmail.com" />
           </div>
         </div>
         <div class="row">
@@ -91,8 +51,8 @@
           </div>
           <div class="form-group col-sm-12 col-md-6">
             <p class="font-weight-bold">Your Profile Image</p>
-          <div class="d-flex">
-          <div class="d-inline-block">
+            <div class="d-flex">
+              <div class="d-inline-block">
                 <img src="{{Auth::user()->image}}" class="img-thumb" alt="user" />
               </div>
               <div class="d-inline-block ml-3 py-2">
@@ -100,8 +60,10 @@
 
                 <label class="text-muted form-control p-2 w-100" for="profileimage" style="cursor:pointer">Choose file</label>
               </div>
+            </div>
           </div>
-          </div>
+        </div>
+        <div class="row">
           <div class="form-group col-sm-12 col-md-6">
             <label for="bio"><strong>Your Short Bio</strong></label>
             <textarea name="bio" id="bio" class="form-control" rows="5" placeholder="type here.."></textarea>
@@ -111,17 +73,18 @@
         <button type="submit" class="btn btn-lg col-sm-12 col-md-3 mt-5">Update Profile</button>
       </form>
     </div>
+
     <!-- account tab -->
-    <div class="tab-pane fade" id="account" role="tabpanel" aria-labelledby="account-tab">
-      <h3 class="mt-5">coming soon...</h3>
+    <div class="tab-pane fade" id="account" role="tabpanel" aria-labelledby="account-tab" id="account">
+      <h3 class="mt-5">Account page coming soon...</h3>
     </div>
     <!-- links tab -->
-    <div class="tab-pane fade" id="links" role="tabpanel" aria-labelledby="links-tab">
-      <h3 class="mt-5">coming soon...</h3>
+    <div class="tab-pane fade" id="links" role="tabpanel" aria-labelledby="links-tab" id="links">
+      <h3 class="mt-5">Links page coming soon...</h3>
     </div>
     <!-- security tab -->
-    <div class="tab-pane fade" id="security" role="tabpanel" aria-labelledby="security-tab">
-      <h3 class="mt-5">coming soon...</h3>
+    <div class="tab-pane fade" id="security" role="tabpanel" aria-labelledby="security-tab" id="security">
+      <h3 class="mt-5">Security page coming soon...</h3>
     </div>
   </div>
 </div>
