73e5d14eac4d4e7d3f90b9d31b4d9383e1c41eb1
JeffOgah <jeff.ogah@gmail.com>
2019-07-17 12:29:29 +0100

    contact page fixes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -15,17 +15,17 @@
     <form class="font-weight-bold mt-4 mb-0" action="">
         <div class="form-group row">
             <div class="col-sm-12 col-md-6">
-                <label for="" class="mb-2 mr-sm-2">Name</label>
-                <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter email" name="email">
+                <label for="name" class="mb-2 mr-sm-2">Name</label>
+                <input type="text" class="form-control mb-2 mr-sm-2" id="name" placeholder="Enter Name" name="name">
             </div>
             <div class="col-sm-12 col-md-6">
-                <label for="" class="mb-2 mr-sm-2">Email</label>
-                <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter password" name="pswd">
+                <label for="email" class="mb-2 mr-sm-2">Email</label>
+                <input type="email" class="form-control mb-2 mr-sm-2" id="email" placeholder="Enter Email" name="email">
             </div>
         </div>
         <div class="form-group mt-4">
-        <label for="">Message</label>
-        <textarea name="" id="" rows="5" class="form-control"></textarea>
+        <label for="message">Message</label>
+        <textarea name="message" id="message" rows="5" class="form-control" placeholder="Enter Message"></textarea>
         <button type="submit" class="btn bg-alt text-white col-sm-12 col-md-3 mt-5">Send Message</button>
         </div>
     </form>
