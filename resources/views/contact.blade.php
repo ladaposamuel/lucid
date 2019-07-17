215fe6f9dfa3ce53bccc152918a63b27a3adf3bc
JeffOgah <jeff.ogah@gmail.com>
2019-07-17 12:24:52 +0100

    fixed contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,51 +3,32 @@
 @parent
 @endsection
 @section('content')
- <!-- Beginning of contact page -->
- 
-<body>
-    <style>
-        .form-inline label {
-            justify-content: left;
-        }
-        .profile-submit-btn {
-        background-color: #871E99;
-        padding: 1rem 2rem;
-        margin-top: 1rem;
-        color: #fff;
-        }
-    </style>
-    
-            <!-- Beginning of Post Content -->
-            <div class="col-lg-8 pb-5">
-                <div class="post-content">
-                    
+<!-- Beginning of contact page -->
 
-                    <div class="container">
-                        <h2>Contact Me</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat accumsan sodales. In
-                            ac euismod augue. Quisque vel porta metus, sit amet aliquam eros.
-                        </p>
-                        <form class="form-inlin" action="/action_page.php">
-                            <div class="form-inline">
-                                <div class="">
-                                    <label for="" class="mb-2 mr-sm-2">Name</label>
-                                    <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter email"
-                                        name="email">
-                                </div>
-                                <div class="">
-                                    <label for="" class="mb-2 mr-sm-2">Email</label>
-                                    <input type="text" class="form-control mb-2 mr-sm-2" id=""
-                                        placeholder="Enter password" name="pswd">
-                                </div>
-                            </div>
-                            <label for="">Message</label>
-                            <textarea name="" id="" cols="30" rows="10" class=" form-text form-control"></textarea>
-                            <button type="submit" class=" profile-submit-btn mb-2">Send Message</button>
-                        </form>
-                    </div>
-                </div>
+<!-- Beginning of Post Content -->
 
- <!-- End of contact page -->
+<div class="container">
+<h4 class="font-weight-bold mb-4">Contact Me</h4>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat accumsan sodales. In ac euismod augue. Quisque vel porta metus, sit amet aliquam eros.
+    </p>
+    <form class="font-weight-bold mt-4 mb-0" action="">
+        <div class="form-group row">
+            <div class="col-sm-12 col-md-6">
+                <label for="" class="mb-2 mr-sm-2">Name</label>
+                <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter email" name="email">
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <label for="" class="mb-2 mr-sm-2">Email</label>
+                <input type="text" class="form-control mb-2 mr-sm-2" id="" placeholder="Enter password" name="pswd">
+            </div>
+        </div>
+        <div class="form-group mt-4">
+        <label for="">Message</label>
+        <textarea name="" id="" rows="5" class="form-control"></textarea>
+        <button type="submit" class="btn bg-alt text-white col-sm-12 col-md-3 mt-5">Send Message</button>
+        </div>
+    </form>
+</div>
+<!-- End of contact page -->
 @endsection
