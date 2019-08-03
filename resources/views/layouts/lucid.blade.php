44a922b2cd60087fb46d27082f05fc255b0a69e9
JeffOgah <jeff.ogah@gmail.com>
2019-08-03 23:50:20 +0100

    following/followers page button

--- resources/views/layouts/lucid.blade.php
+++ resources/views/layouts/lucid.blade.php
@@ -117,16 +117,16 @@
         <div class="user-stats text-center mt-3 pb-0">
           <div class="d-inline-block">
             @if (empty($fcount))
-            <a href="#" onclick="showData('following')" class="pr-2">0 <br /> <small class="text-muted">Following</small></a>
+            <a href="/{{ $user->username}}#following" onclick="reload()" class="pr-2">0 <br /> <small class="text-muted">Following</small></a>
             @else
-            <a href="#" onclick="showData('following')" class="pr-2">{{$fcount}} <br /> <small class="text-muted">Following</small></a>
+            <a href="/{{ $user->username}}#following" onclick="reload()" class="pr-2">{{$fcount}} <br /> <small class="text-muted">Following</small></a>
             @endif
           </div>
           <div class="d-inline-block">
             @if (empty($count))
-            <a href="#" onclick="showData('follower')">0 <br /> <small class="text-muted">Followers</small></a>
+            <a href="/{{ $user->username}}#followers" onclick="reload()">0 <br /> <small class="text-muted">Followers</small></a>
             @else
-            <a href="#" onclick="showData('follower')">{{$count}} <br /> <small class="text-muted">Followers</small></a>
+            <a href="/{{ $user->username}}#followers" onclick="reload()">{{$count}} <br /> <small class="text-muted">Followers</small></a>
             @endif
           </div>
           <div class="mt-3">
@@ -167,7 +167,7 @@
 
         <!-- Beginning of Post Content -->
         @yield('content')
-        <!-- Do not delete, holding uservalue for JS -->
+        <!-- Do not delete, holding username value for JS -->
         <input type="hidden" value="{{ $user->username }}" id="username">
       </div>
 
@@ -176,9 +176,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script>
-    function showData(e) {
-      alert(location)
-    }
+  function reload() {
+    document.location.reload()
+  }
   </script>
 </body>
 
