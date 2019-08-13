72fa69f635a922e0bbc5bb2632a95b8834a0d3c2
JeffOgah <jeff.ogah@gmail.com>
2019-08-13 05:49:31 +0100

    edited default contact message

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,8 +57,7 @@
         </div>
         <div class="form-group mt-4">
         <label for="message">Edit Display Message</label>
-        <textarea name="message" id="message" rows="5" class="form-control" placeholder="Cannot Be Blank">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat accumsan sodales. In ac euismod augue. Quisque vel porta metus, sit amet aliquam eros.
-        </textarea>
+        <textarea name="message" id="message" rows="5" class="form-control" placeholder="Cannot Be Blank">You can use this form to contact me.</textarea>
         <span class="text-danger" id="msgError" style="display:none;"></span>
         <button type="submit" name="editContactDetails" class="btn bg-alt text-white col-sm-12 col-md-3 mt-5">Save</button>
         </div>
